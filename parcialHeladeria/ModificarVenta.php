97e74161cfef6da3fd1d523d88c0a2c87e5552c1
andreaibt21 <andreaixabella@gmail.com>
2022-10-17 17:34:36 -0300

    eliminar comentarios

--- parcialHeladeria/ModificarVenta.php
+++ parcialHeladeria/ModificarVenta.php
@@ -9,55 +9,32 @@ include_once "helado.php";
 include_once "GuardarOLeerJson.php";
 $datos = json_decode(file_get_contents("php://input"), true);
 
- $numeroDePedido = $datos["numeroDePedido"];
- $email = $datos["email"];
- $tipo = $datos["tipo"];
- $sabor = $datos["sabor"];
- $stock = $datos["stock"];
+$numeroDePedido = $datos["numeroDePedido"];
+$email = $datos["email"];
+$tipo = $datos["tipo"];
+$sabor = $datos["sabor"];
+$stock = $datos["stock"];
 
 $rutaVentas = "Ventas.json";
 $arrayVentas = GuardarOLeerJson::LeerDeJson($rutaVentas, false);
 
 
 
-    foreach ($arrayVentas as $venta) {
-        $usuarioAux = Herramientas::SacarValorDeLaClave($venta, "_usuario");
-        $emailAux = Herramientas::SacarValorDeLaClave($usuarioAux, "_email");
-        $numeroDePedidoAux = Herramientas::SacarValorDeLaClave($venta, "numeroDePedido");
-        if (
-            strcmp(   $numeroDePedidoAux, $numeroDePedido) == 0 &&
-            strcmp( $emailAux, $email) == 0
-        ) {
-            echo "<br>Datos ingresados: " . $numeroDePedido . "   " . $email . "  " . $tipo . "   " . $stock. "<br>";
-            $venta->_sabor = $datos["sabor"];
-            $venta->_tipo = $datos["tipo"];
-            $venta->_stock = $datos["stock"];
-            echo "\nSe modificó la venta\n";
-        }
-
-
-        //     if (
-        //         strcmp($numeroDePedidoAux, $numeroDePedido) == 0
-        //     ) {
-        //         echo "Datos ingresados: " . $numeroDePedido . " " . $email . " " . $tipo . " " . $stock;
-
-        //     }else{
-        //         echo "aaaaa";echo "<br>";
-        //     }
-            
-        //     if (
-            
-        //         strcmp($emailAux, $email) == 0
-        //     ) {
-        //         echo "Datos ingresados: " . $numeroDePedido . " " . $email . " " . $tipo . " " . $stock;
-        
-        //     }else{
-        
-        //     echo "rrrrrr";echo "<br>";
-        //     }
-        //     echo "<br>";
-
+foreach ($arrayVentas as $venta) {
+    $usuarioAux = Herramientas::SacarValorDeLaClave($venta, "_usuario");
+    $emailAux = Herramientas::SacarValorDeLaClave($usuarioAux, "_email");
+    $numeroDePedidoAux = Herramientas::SacarValorDeLaClave($venta, "numeroDePedido");
+    if (
+        strcmp($numeroDePedidoAux, $numeroDePedido) == 0 &&
+        strcmp($emailAux, $email) == 0
+    ) {
+        echo "<br>Datos ingresados: " . $numeroDePedido . "   " . $email . "  " . $tipo . "   " . $stock . "<br>";
+        $venta->_sabor = $datos["sabor"];
+        $venta->_tipo = $datos["tipo"];
+        $venta->_stock = $datos["stock"];
+        echo "\nSe modificó la venta\n";
     }
+}
 
 
-    GuardarOLeerJson::GuardarEnJson($arrayVentas, "Ventas.json");
+GuardarOLeerJson::GuardarEnJson($arrayVentas, "Ventas.json");
